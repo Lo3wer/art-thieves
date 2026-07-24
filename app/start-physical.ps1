9b4abd281264abea30598725c8b019b617a485d9
Lo3wer <leozhang226@gmail.com>
2026-07-24 03:27:14 -0700

    removed physical device ps script

--- app/start-physical.ps1
+++
@@ -1,3 +0,0 @@
-$ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -like "192.*" -or $_.IPAddress -like "10.*" } | Select-Object -First 1).IPAddress
-$env:EXPO_PUBLIC_API_URL = "http://$($ip):3001"
-npx expo start
