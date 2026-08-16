be1d32614a221e915f231feb3f21a658217855f9
Lo3wer <leozhang226@gmail.com>
2026-08-16 10:48:33 -0700

    feat: add /healthz endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(getUploadsDir()));
 
+app.get('/healthz', (_req, res) => res.json({ ok: true }));
+
 app.use('/api', routes);
 
 setIO(io);
